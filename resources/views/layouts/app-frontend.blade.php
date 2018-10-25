2ad274c677b444869ad2f3f5a31012386683243d
rafif favian <rafiffavian1498@gmail.com>
2018-10-25 10:16:37 +0700

    new

--- resources/views/layouts/app-frontend.blade.php
+++ resources/views/layouts/app-frontend.blade.php
@@ -8,7 +8,7 @@
 <html lang="zxx">
 
 <head>
-    <title>Alter Team</title>
+    <title>Crayon Sinchan</title>
 
     <!-- Meta tag Keywords -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
